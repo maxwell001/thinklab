76c95cb94847104ba7cab1a395c914283603e584
mamy <mamy@Think-PC>
2014-11-30 22:06:05 +0800

    init

---
+++ activemq5.9/src/com/activemq/test/Consumer.java
@@ -0,0 +1,43 @@
+package com.activemq.test;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+public class Consumer {
+	private static String brokerURL = "tcp://localhost:61616";  
+	private static ConnectionFactory factory;  
+	private Connection connection;  
+	private Session session;  
+	  
+	public Consumer() throws JMSException {  
+	    factory = new ActiveMQConnectionFactory(brokerURL);  
+	    connection = factory.createConnection();  
+	    connection.start();  
+	    session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);  
+	}  
+	  
+	public void close() throws JMSException {  
+	    if (connection != null) {  
+	        connection.close();  
+	    }  
+	}      
+	  
+	public static void main(String[] args) throws JMSException {  
+	    Consumer consumer = new Consumer(); 
+	    String[] message = {"test"};
+	    for (String stock : message) {  
+	        Destination destination = consumer.getSession().createTopic("STOCKS." + stock);  
+	        MessageConsumer messageConsumer = consumer.getSession().createConsumer(destination);  
+	        messageConsumer.setMessageListener(new Listener());  
+	    }  
+	}  
+	  
+	public Session getSession() {  
+	    return session;  
+	}  
+}
