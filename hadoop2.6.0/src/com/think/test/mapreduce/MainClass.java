ba31d6b81384f18d74e7f047ebd6016c8dd5a587
mamy <mamy@Think-PC>
2015-05-17 13:47:08 +0800

    init

--- hadoop2.6.0/src/com/think/test/mapreduce/MainClass.java
+++ hadoop2.6.0/src/com/think/test/mapreduce/MainClass.java
@@ -33,7 +33,7 @@ public class MainClass {
 		List<String> otherArgs = new ArrayList<String>();
 		for (int i=0; i < remainingArgs.length; ++i) {
 			if ("-skip".equals(remainingArgs[i])) {
-				job.addCacheFile(new Path(remainingArgs[++i]).toUri());
+//				job.addCacheFile(new Path(remainingArgs[++i]).toUri());
 				job.getConfiguration().setBoolean("wordcount.skip.patterns", true);
 			} else {
 				otherArgs.add(remainingArgs[i]);
